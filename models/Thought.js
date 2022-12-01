13870c8ceb0ec8d7b6015a8bbb9a0235800e707e
JK code <112551853+jkanvision@users.noreply.github.com>
2022-11-30 21:46:35 -0500

    initial edits to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -4,9 +4,21 @@ const dateFormat = require("../utils/dateFormat");
 
 const thoughtSchema = new Schema(
     {
-        thoughtText: {},
-        createdAt: {},
-        username: {},
+        thoughtText: {
+            type: String,
+            reuired: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // getter to format time
+        },
+        username: {
+            type: String,
+            required: true,
+        },
         reactions: [reactionSchema],
     },
     {
