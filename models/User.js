7bc2f27e62b5127f7f4f82bc6db0b766430b854a
JK code <112551853+jkanvision@users.noreply.github.com>
2022-11-30 21:09:28 -0500

    adds data.js; edits userSchema in User.js

--- models/User.js
+++ models/User.js
@@ -3,16 +3,29 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema(
     {
         username: {
-        
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
         },
-        email: {
 
+        email: {
+            type: String,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                }
+            },
+            required: [true, "Email required"],
+            unique: true,
         },
+
         thoughts: [
             {
                 // references the Thought ObjectId
             },
         ],
+
         friends: [
             {
                 // references the User ObjectId
---
+++ utils/data.js
@@ -0,0 +1,22 @@
+const names = [
+    "David",
+    "Lucy",
+    "Mary",
+    "Tasha",
+    "Jose",
+];
+
+const thoughts = [
+    "I wish I could go back in time!",
+    "I'm having a great day ;)",
+    "I prefer to play sports, not watch them.",
+    "Puppies are the best!",
+    "We should all be so lucky...",
+];
+
+const reactions = [
+    "I agree",
+    "I disagree",
+];
+
+const genRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
