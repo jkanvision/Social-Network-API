88dfdaaa3f46d282e287cc5e41f9f5a016a0dda1
JK code <112551853+jkanvision@users.noreply.github.com>
2022-11-30 22:06:30 -0500

    initial edits to Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -3,11 +3,30 @@ const dateFormat = require("../utils/dateFormat");
 
 const reactionSchema = new Schema(
     {
-        reactionId: {},
-        reactionBody: {},
-        username: {},
-        createdAt: {},
+        reactionId: {
+            _id: mongoose.ObjectId,
+            
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            minLength: 1,
+            mazLength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // getter to format time
+        },
     },
+
     {
         toJSON: {
             getters: true
